88dec267d8fe0078d926808c9b4575c28059d6e9
Milan Patel <milan1542000@gmail.com>
2024-02-15 18:59:16 +0530

    Modified add method and added rspecs for string containing more than 2 numbers with delimiter as a comma

--- string_calc.rb
+++ string_calc.rb
@@ -1,7 +1,11 @@
 class StringCalc
   def self.add(string)
-    if string.strip.empty?
-      return 0
+    string = string.strip
+    if string.empty?
+      return 0           # Returns 0 when empty string is passed or the string with just empty space is passed
+    else
+      string_arr = string.split(',').map(&:to_i)      #converts the string to array of integers
+      return string_arr.sum       # Returns sum of numbers present into the string
     end
   end
 end
--- string_calc_spec.rb
+++ string_calc_spec.rb
@@ -10,5 +10,12 @@ RSpec.describe StringCalc do
         expect(StringCalc.add(" ")).to eq(0)
       end
     end
+
+    context "when string with more than 2 numbers is passed" do
+      it "returns the sum of those 2 numbers" do
+        expect(StringCalc.add("1, 2")).to eq(3)
+        expect(StringCalc.add("3, 4, 6, 10")).to eq(23)
+      end
+    end
   end
 end
