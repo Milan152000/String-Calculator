f62cd72dd2900901bb347b7c9b0f57ee6016cc4f
Milan Patel <milan1542000@gmail.com>
2024-02-15 21:00:22 +0530

    Modified code to raise error when negative integers are present into the string and also added rspecs.

--- string_calc.rb
+++ string_calc.rb
@@ -4,9 +4,19 @@ class StringCalc
     if string.empty?
       return 0           # Returns 0 when empty string is passed or the string with just empty space is passed
     else
-      delimiter = /[,\n;]/
+      delimiter = /[,\n; ]/        # Given the delimiters as space, comma, semi colon, and new line
+      regex = /\s*-?\d+\s*/         # Regex to match the number inside the string
+
       string_arr = string.split(delimiter)        # This will split the string with multiple delimiters
-      sum = string_arr.map(&:to_i).sum        # Convert the array of strings to an array of integers and calculate the sum
+      string_arr = string_arr.map {|x| x.match(regex)}.compact        # Match the numbers and removes nil elements
+      string_arr = string_arr.map {|y| y[0].to_i}         # Maps each element to integer
+      numbers = string_arr.map(&:to_i)        # Convert the array of strings to an array of integers
+
+      # Raise an error if negative numbers are present into the string
+      negative_numbers = numbers.select {|x| x < 0 }
+      raise ArgumentError, "Negative numbers are not allowed: #{negative_numbers.join(',')}" if negative_numbers.any?
+
+      sum = numbers.sum
     end
   end
 end
--- string_calc_spec.rb
+++ string_calc_spec.rb
@@ -12,16 +12,29 @@ RSpec.describe StringCalc do
     end
 
     context "when string with more than 2 numbers is passed" do
-      it "returns the sum of those 2 numbers" do
-        expect(StringCalc.add("1, 2")).to eq(3)
-        expect(StringCalc.add("3, 4, 6, 10")).to eq(23)
+      context "with a single delimiter as a comma" do
+        it "returns the sum of those 2 numbers" do
+          expect(StringCalc.add("1, 2")).to eq(3)
+          expect(StringCalc.add("3, 4, 6, 10")).to eq(23)
+        end
       end
-    end
 
-    context "when string with more than 2 numbers with multiple delimiters is passed" do
-      it "returns the sum of all numbers" do
-        expect(StringCalc.add("1, 2; 3\n 5")).to eq(11)
-        expect(StringCalc.add("3\n4, 6; 10")).to eq(23)
+      context "with multiple delimiters" do
+        context "with all the positive numbers inside it" do
+          it "returns the sum of all numbers" do
+            expect(StringCalc.add("1, 2; 3\n 5")).to eq(11)
+            expect(StringCalc.add("3\n4, 6; 10")).to eq(23)
+            expect(StringCalc.add("//1,2;3\n6\n\n8,asdvr  11; jhfdv5")).to eq(36)
+            expect(StringCalc.add("//2,4;3\n7\n\n8,asdvr 15 10; jhfdv")).to eq(49)
+          end
+        end
+
+        context "with some of the negative numbers inside it" do
+          it "raises an exception with the array of negative numbers" do
+            expect {StringCalc.add("//1,--2;3\n-6\n\n8,asdvr  11; jhfdv5")}.to raise_error(ArgumentError, "Negative numbers are not allowed: -2,-6")
+            expect {StringCalc.add("//-2,4;3\n-7\n\n8,asdvr -15 10; jhfdv")}.to raise_error(ArgumentError, "Negative numbers are not allowed: -2,-7,-15")
+          end
+        end
       end
     end
   end
